61e73d40b5e5824d005eb7c948f6086faf49a0ad
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-08 14:11:53 -0400

    Initial push. Created the calculator.

--- src/main/java/com/pluralsight/basiccalculator.java
+++ src/main/java/com/pluralsight/basiccalculator.java
@@ -6,6 +6,18 @@ public class basiccalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a number: ");
+        float numOne = scanner.nextFloat();
+
+        System.out.println("Enter another number: ");
+        float numTwo = scanner.nextFloat();
+
+        float total = numOne + numTwo;
+
+        System.out.printf("Total is:  %2f", total);
+
+
+
 
     }
 }
