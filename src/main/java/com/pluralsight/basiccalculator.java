27be70898f06cb2cdbb794c0c8ef974e86c84dfc
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-08 14:02:42 -0400

    Initial push.

---
+++ src/main/java/com/pluralsight/basiccalculator.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class basiccalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
